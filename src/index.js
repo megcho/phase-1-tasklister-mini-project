ca82bbc5ec2fe5e2a85c2f475be47419648b3c42
megan cho <meganlisacho@gmail.com>
2021-05-03 22:23:44 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+// });
+
+const myForm = document.getElementById("create-task-form")
+
+function handleDelete(e){
+  e.target.parentElement.remove()
+}
+
+function listenForSubmit(e){
+  e.preventDefault()
+  const newTask = e.target["new-task-description"].value
+  const taskUL = document.querySelector("#tasks")
+  const deleteButton = document.createElement("button")
+  deleteButton.innerText = "X"
+  deleteButton.addEventListener("click", handleDelete)
+  const LI = document.createElement("li")
+  LI.textContent = newTask
+  LI.append(deleteButton)
+  taskUL.appendChild(LI)
+  e.target.reset()
+}
+
+
+myForm.addEventListener("submit", listenForSubmit)
